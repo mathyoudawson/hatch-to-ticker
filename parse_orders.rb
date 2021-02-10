3395eb732d00a2af0a3b1415388c88f200714b6c
Matthew Dawson <matthewdawson@outlook.com>
2021-02-10 21:28:54 +1300

    Remove Grouping (Ticker supports multiple cost basis lots)

--- parse_orders.rb
+++ parse_orders.rb
@@ -13,21 +13,13 @@ class ParseOrders
       quantity = order["Quantity"].to_f
       unit_cost = order["Price"].to_f
 
-      existing_lot = ticker_hash["lots"].find { |lot| lot["symbol"] == symbol } #remove it and add later?
-
-      if existing_lot
-        quantity += existing_lot["quantity"].to_f
-        unit_cost = (unit_cost + existing_lot["unit_cost"].to_f) / 2.0
-        ticker_hash["lots"].delete(existing_lot)
-      end
-
       lot_hash = {
         "symbol" => symbol,
         "quantity" => quantity,
         "unit_cost" => unit_cost
       }
 
-      ticker_hash["watchlist"] << order["Instrument Code"] unless existing_lot
+      ticker_hash["watchlist"] << order["Instrument Code"]
       ticker_hash["lots"] << lot_hash
     end
 
